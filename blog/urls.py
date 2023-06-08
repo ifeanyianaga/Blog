9a7c48f3a4ee33eaefc8e93b338ef8e4d8983265
ifeanyi anaga <ifeanyianaga95@gmail.com>
2023-06-08 15:12:59 +0100

    like button

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 	path('<str:slug>/',views.blog_post_detail_view,name="detail"),
 	path('<str:slug>/like/',PostLikeToggle.as_view(), name="like"),
 	path('<str:slug>/getCount/',PostLikeDB.as_view(), name="getCount"),
-	path('<str:slug>/getUsers/',PostLikeUsers.as_view(), name="getUsers"),
+	#path('<str:slug>/getUsers/',PostLikeUsers.as_view(), name="getUsers"),
 
 	#path('<str:slug>/comment/',views.comment_view,name="comment"),
 	path('<str:slug>/comment/edit/<uuid:comment_id>/',views.update_comment,name="update_comment"),
